2fefde076798fed08afb4ea0349493b1d0427187
[duarte dornellas] <[duarte_dornellas@hotmail.com]>
2018-09-12 16:24:49 +0100

    matching

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # features_benchmark
-This repository contains code for the runtime benchmarking of feature detection, description and matching  with OpenCV 3.3.1 implementations. Two output files are generated, one for the feature detectors, and one for the feature descriptors (computed on FAST features). Each line corresponds to an iteration, and is formatted as follows:
+This repository contains code for the runtime benchmarking of feature detection, description and matching  with OpenCV 3.3.1 implementations. Three output files are generated, 'results_detectors.txt', 'results_descriptors.txt' and 'results_matchers.txt'. For the description and matching tests, the descriptors were computed on FAST features. On these files, each line corresponds to an iteration, and is formatted as follows:
 
 'results_detectors.txt':	Shi-Tomasi	SIFT	MSER	FAST	SURF	BRISK	ORB
 
-'results_descriptors.txt': 	SIFT	SURF	BRIEF	BRISK	ORB	FREAK
+'results_descriptors.txt': 	SIFT	SURF	BRIEF	BRISK	ORB   FREAK
 
+'results_matchers.txt': 	SIFT	SURF	BRIEF	BRISK	ORB   FREAK
--- features_benchmark.cpp
+++ features_benchmark.cpp
@@ -10,10 +10,14 @@ using namespace cv;
 using namespace cv::xfeatures2d;
 
 // Global variable declaration
-const Mat img_EuRoC = imread( "Images/EuRoC/1403636579763555584.png", IMREAD_GRAYSCALE );
-const Mat img_KITTI = imread( "Images/KITTI/000000.png", IMREAD_GRAYSCALE );
-const Mat img_PennCOSYVIO = imread( "Images/PennCOSYVIO/frame_0001.png", IMREAD_GRAYSCALE );
-const Mat img_TUMVI = imread( "Images/TUMVI/1520531829251142058.png", IMREAD_GRAYSCALE );
+const Mat img_EuRoC_0 = imread( "Images/EuRoC/1403636579763555584.png", IMREAD_GRAYSCALE );
+const Mat img_EuRoC_1 = imread( "Images/EuRoC/1403636579813555456.png", IMREAD_GRAYSCALE );
+const Mat img_KITTI_0 = imread( "Images/KITTI/000000.png", IMREAD_GRAYSCALE );
+const Mat img_KITTI_1 = imread( "Images/KITTI/000001.png", IMREAD_GRAYSCALE );
+const Mat img_PennCOSYVIO_0 = imread( "Images/PennCOSYVIO/frame_0001.png", IMREAD_GRAYSCALE );
+const Mat img_PennCOSYVIO_1 = imread( "Images/PennCOSYVIO/frame_0002.png", IMREAD_GRAYSCALE );
+const Mat img_TUMVI_0 = imread( "Images/TUMVI/1520531829251142058.png", IMREAD_GRAYSCALE );
+const Mat img_TUMVI_1 = imread( "Images/TUMVI/1520531829301144058.png", IMREAD_GRAYSCALE );
 
 // Function 'mean_time_detectors'
 template<class type> float mean_time_detectors(Ptr<type> &detector){
@@ -22,10 +26,10 @@ template<class type> float mean_time_detectors(Ptr<type> &detector){
                           keypoints_PennCOSYVIO,
                           keypoints_TUMVI;
     auto start = std::chrono::high_resolution_clock::now();
-    detector->detect( img_EuRoC, keypoints_EuRoC );
-    detector->detect( img_KITTI, keypoints_KITTI );
-    detector->detect( img_PennCOSYVIO, keypoints_PennCOSYVIO );
-    detector->detect( img_TUMVI, keypoints_TUMVI );
+    detector->detect( img_EuRoC_0, keypoints_EuRoC );
+    detector->detect( img_KITTI_0, keypoints_KITTI );
+    detector->detect( img_PennCOSYVIO_0, keypoints_PennCOSYVIO );
+    detector->detect( img_TUMVI_0, keypoints_TUMVI );
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = finish - start;
     int n_keypoints = keypoints_EuRoC.size() +
@@ -48,13 +52,16 @@ template<class type> float mean_time_descriptors(Ptr<type> &descriptor){
     std::vector<KeyPoint> keypoints_EuRoC, keypoints_KITTI, keypoints_PennCOSYVIO, keypoints_TUMVI;
     Mat descriptors_EuRoC, descriptors_KITTI, descriptors_PennCOSYVIO, descriptors_TUMVI;
 
-    detector_FAST->detect( img_EuRoC, keypoints_EuRoC );
-    detector_FAST->detect( img_KITTI, keypoints_KITTI );
-    detector_FAST->detect( img_PennCOSYVIO, keypoints_PennCOSYVIO );
-    detector_FAST->detect( img_TUMVI, keypoints_TUMVI );
+    detector_FAST->detect( img_EuRoC_0, keypoints_EuRoC );
+    detector_FAST->detect( img_KITTI_0, keypoints_KITTI );
+    detector_FAST->detect( img_PennCOSYVIO_0, keypoints_PennCOSYVIO );
+    detector_FAST->detect( img_TUMVI_0, keypoints_TUMVI );
 
     auto start = std::chrono::high_resolution_clock::now();
-    descriptor->compute( img_EuRoC, keypoints_EuRoC, descriptors_EuRoC );
+    descriptor->compute( img_EuRoC_0, keypoints_EuRoC, descriptors_EuRoC );
+    descriptor->compute( img_KITTI_0, keypoints_KITTI, descriptors_KITTI );
+    descriptor->compute( img_PennCOSYVIO_0, keypoints_PennCOSYVIO, descriptors_PennCOSYVIO);
+    descriptor->compute( img_TUMVI_0, keypoints_TUMVI, descriptors_TUMVI );
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = finish - start;
     int n_keypoints = keypoints_EuRoC.size() +
@@ -70,11 +77,77 @@ template<class type> float mean_time_descriptors(Ptr<type> &descriptor){
     return mean_runtime;
 }
 
+// Function 'mean_time_matchers'
+template<class type> float mean_time_matchers(Ptr<type> &descriptor){
+    Ptr<FastFeatureDetector> detector_FAST = FastFeatureDetector::create();
+
+    std::vector<KeyPoint> keypoints_EuRoC_0, keypoints_KITTI_0,
+                          keypoints_PennCOSYVIO_0, keypoints_TUMVI_0,
+                          keypoints_EuRoC_1, keypoints_KITTI_1,
+                          keypoints_PennCOSYVIO_1, keypoints_TUMVI_1;
+    Mat descriptors_EuRoC_0, descriptors_KITTI_0,
+        descriptors_PennCOSYVIO_0, descriptors_TUMVI_0,
+        descriptors_EuRoC_1, descriptors_KITTI_1,
+        descriptors_PennCOSYVIO_1, descriptors_TUMVI_1;
+
+    BFMatcher matcher;
+    std::vector< DMatch > matches_EuRoC, matches_KITTI,
+                          matches_PennCOSYVIO, matches_TUMVI;
+
+    detector_FAST->detect( img_EuRoC_0, keypoints_EuRoC_0 );
+    detector_FAST->detect( img_KITTI_0, keypoints_KITTI_0 );
+    detector_FAST->detect( img_PennCOSYVIO_0, keypoints_PennCOSYVIO_0 );
+    detector_FAST->detect( img_TUMVI_0, keypoints_TUMVI_0 );
+
+    detector_FAST->detect( img_EuRoC_1, keypoints_EuRoC_1 );
+    detector_FAST->detect( img_KITTI_1, keypoints_KITTI_1 );
+    detector_FAST->detect( img_PennCOSYVIO_1, keypoints_PennCOSYVIO_1 );
+    detector_FAST->detect( img_TUMVI_1, keypoints_TUMVI_1 );
+
+    descriptor->compute( img_EuRoC_0, keypoints_EuRoC_0, descriptors_EuRoC_0 );
+    descriptor->compute( img_KITTI_0, keypoints_KITTI_0, descriptors_KITTI_0 );
+    descriptor->compute( img_PennCOSYVIO_0, keypoints_PennCOSYVIO_0, descriptors_PennCOSYVIO_0);
+    descriptor->compute( img_TUMVI_0, keypoints_TUMVI_0, descriptors_TUMVI_0 );
+
+    descriptor->compute( img_EuRoC_1, keypoints_EuRoC_1, descriptors_EuRoC_1 );
+    descriptor->compute( img_KITTI_1, keypoints_KITTI_1, descriptors_KITTI_1 );
+    descriptor->compute( img_PennCOSYVIO_1, keypoints_PennCOSYVIO_1, descriptors_PennCOSYVIO_1);
+    descriptor->compute( img_TUMVI_1, keypoints_TUMVI_1, descriptors_TUMVI_1 );
+
+    auto start = std::chrono::high_resolution_clock::now();
+    matcher.match( descriptors_EuRoC_0, descriptors_EuRoC_1, matches_EuRoC );
+    matcher.match( descriptors_KITTI_0, descriptors_KITTI_1, matches_KITTI );
+    matcher.match( descriptors_PennCOSYVIO_0, descriptors_PennCOSYVIO_1, matches_PennCOSYVIO );
+    matcher.match( descriptors_TUMVI_0, descriptors_TUMVI_1, matches_TUMVI );
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+
+    int n_keypoints = keypoints_EuRoC_0.size() + keypoints_EuRoC_1.size() +
+                      keypoints_KITTI_0.size() + keypoints_KITTI_1.size() +
+                      keypoints_PennCOSYVIO_0.size() + keypoints_PennCOSYVIO_1.size() +
+                      keypoints_TUMVI_0.size() + keypoints_TUMVI_1.size();
+
+    int n_matches = matches_EuRoC.size() +
+                    matches_KITTI.size() +
+                    matches_PennCOSYVIO.size() +
+                    matches_TUMVI.size();
+    float mean_runtime = elapsed.count() / n_matches;
+    std::cout << std::endl
+              << "Descriptor type: " << typeid(type).name() << std::endl
+              << "Number of keypoints: " << n_keypoints << '\n'
+              << "Elapsed time: " << elapsed.count() << " s\n"
+              << "Mean computation time per match: " << mean_runtime << " s\n";
+    return mean_runtime;
+}
+
 // Function 'main'
 int main( int argc, char** argv )
 {
     // Check for image loading errors
-    if( !img_EuRoC.data || !img_KITTI.data || !img_PennCOSYVIO.data || !img_TUMVI.data )
+    if( !img_EuRoC_0.data || !img_KITTI_0.data ||
+        !img_PennCOSYVIO_0.data || !img_TUMVI_0.data ||
+        !img_EuRoC_1.data || !img_KITTI_1.data ||
+        !img_PennCOSYVIO_1.data || !img_TUMVI_1.data )
     { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
     // Initialize feature detectors:
@@ -94,11 +167,12 @@ int main( int argc, char** argv )
     Ptr<ORB> descriptor_ORB = ORB::create();
     Ptr<FREAK> descriptor_FREAK = FREAK::create();
 
-    // Compute elapsed and mean runtime per keypoint detection (10 iterations)
-    std::ofstream results_detectors, results_descriptors;
+    // Compute elapsed and mean runtime per keypoint detection
+    int iterations = 10;
+    std::ofstream results_detectors, results_descriptors, results_matchers;
 
     results_detectors.open ("results_detectors.txt");
-    for(int i=0; i<10; i++){
+    for(int i=0; i<iterations; i++){
         results_detectors << mean_time_detectors<GFTTDetector>(detector_GFTT) << '\t'
                           << mean_time_detectors<SIFT>(detector_SIFT) << '\t'
                           << mean_time_detectors<MSER>(detector_MSER) << '\t'
@@ -109,9 +183,9 @@ int main( int argc, char** argv )
     }
     results_detectors.close();
 
-    // Compute elapsed and mean runtime per keypoint description (10 iterations)
+    // Compute elapsed and mean runtime per keypoint description
     results_descriptors.open ("results_descriptors.txt");
-    for(int i=0; i<10; i++){
+    for(int i=0; i<iterations; i++){
         results_descriptors << mean_time_descriptors<SIFT>(descriptor_SIFT) << '\t'
                             << mean_time_descriptors<SURF>(descriptor_SURF) << '\t'
                             << mean_time_descriptors<BriefDescriptorExtractor>(descriptor_BRIEF) << '\t'
@@ -120,6 +194,18 @@ int main( int argc, char** argv )
                             << mean_time_descriptors<FREAK>(descriptor_FREAK) << '\n';
     }
     results_descriptors.close();
+
+    // Compute elapsed and mean runtime per keypoint match
+    results_matchers.open ("results_matchers.txt");
+    for(int i=0; i<iterations; i++){
+        results_matchers << mean_time_matchers<SIFT>(descriptor_SIFT) << '\t'
+                         << mean_time_matchers<SURF>(descriptor_SURF) << '\t'
+                         << mean_time_matchers<BriefDescriptorExtractor>(descriptor_BRIEF) << '\t'
+                         << mean_time_matchers<BRISK>(descriptor_BRISK) << '\t'
+                         << mean_time_matchers<ORB>(descriptor_ORB) << '\t'
+                         << mean_time_matchers<FREAK>(descriptor_FREAK) << '\n';
+    }
+    results_matchers.close();
 
     return 0;
 }
